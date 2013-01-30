319b67566f89f7553d081c63dad20e91171ede04
ruby-developer <mieno.masayuki@adniss.jp>
2013-01-30 11:15:47 +0900

    コメント追加

--- src/jp/lg/ishinomaki/city/mrs/Consts.java
+++ src/jp/lg/ishinomaki/city/mrs/Consts.java
@@ -1,11 +1,45 @@
+//
+//  Consts.java
+//  LGDIS-Input-Tools
+//
+//  Copyright (C) 2012 ISHINOMAKI CITY OFFICE.
+//
+//
+
 package jp.lg.ishinomaki.city.mrs;
 
+/**
+ * アプリケーション全体の設定値
+ * 
+ */
 public class Consts {
 
+    // -----------------------------------------------------
+    // キューに登録する際にデータのヘッダに付与するデータ種別
+    // -----------------------------------------------------
+    /**
+     * XML
+     */
     public static final String QUEUE_DATA_TYPE_XML = "XML";
-    public static final String QUEUE_DATA_TYPE_TXT = "TXT";
-    public static final String QUEUE_DATA_TYPE_PDF = "PDF";
-    public static final String QUEUE_DATA_TYPE_BUF = "BUF";
-    public static final String QUEUE_DATA_TYPE_GRI = "GRI";
     
+    /**
+     * TXT
+     */
+    public static final String QUEUE_DATA_TYPE_TXT = "TXT";
+    
+    /**
+     * PDF
+     */
+    public static final String QUEUE_DATA_TYPE_PDF = "PDF";
+    
+    /**
+     * BUF
+     */
+    public static final String QUEUE_DATA_TYPE_BUF = "BUF";
+    
+    /**
+     * GRI
+     */
+    public static final String QUEUE_DATA_TYPE_GRI = "GRI";
+
 }
