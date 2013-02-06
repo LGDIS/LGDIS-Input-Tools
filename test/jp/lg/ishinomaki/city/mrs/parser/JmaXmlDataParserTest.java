fd3bf600b061762b7e4adeb6969609c0cd263077
ruby-developer <mieno.masayuki@adniss.jp>
2013-02-06 18:33:14 +0900

    テストロジック追加

--- test/jp/lg/ishinomaki/city/mrs/parser/JmaXmlDataParserTest.java
+++ test/jp/lg/ishinomaki/city/mrs/parser/JmaXmlDataParserTest.java
@@ -3,13 +3,22 @@ package jp.lg.ishinomaki.city.mrs.parser;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
-public class JmaDataParserTest {
+public class JmaXmlDataParserTest {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
@@ -32,6 +41,37 @@ public class JmaDataParserTest {
         String actual = "abc";
         String expected = "def";
         assertThat(actual, is(expected));
+    }
+    
+    @Test
+    public void Control部取得成功() {
+        // テスト用XML
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Report xmlns=\"http://xml.kishou.go.jp/jmaxml1/\" xmlns:jmx=\"http://xml.kishou.go.jp/jmaxml1/\"><Control><Title>季節観測</Title></Control></Report>";
+        // テストターゲットクラス
+        JmaXmlDataParser target = new JmaXmlDataParser();
+        
+        InputStream bis = new ByteArrayInputStream(xml.getBytes());
+        DocumentBuilder db;
+        try {
+            db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = db.parse(bis);
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            JmaParseRule rule = JmaParseRule.getInstance();
+            
+            target.parseXmlControl(doc, xpath, rule);
+            String actual = target.getXmlControl();
+            assertThat(actual, is("<Control><Title>季節観測</Title></Control>"));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+    
+    public void Control部なし() {
+        
+    }
+    
+    public void Control部取得失敗() {
+        
     }
 
 }
