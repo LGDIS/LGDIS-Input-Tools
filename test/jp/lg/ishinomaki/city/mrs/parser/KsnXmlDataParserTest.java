d6ff58113943868e9d26d2b3519bb94dcd84a04a
ruby-developer <mieno.masayuki@adniss.jp>
2013-02-07 23:08:14 +0900

    テストケース追加

--- test/jp/lg/ishinomaki/city/mrs/parser/KsnXmlDataParserTest.java
+++ test/jp/lg/ishinomaki/city/mrs/parser/KsnXmlDataParserTest.java
@@ -2,16 +2,28 @@ package jp.lg.ishinomaki.city.mrs.parser;
 
 import static org.junit.Assert.*;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
 public class KsnXmlDataParserTest {
 
+    static XPath xpath;
+    static Document doc;
+    static KsnParseRule rule;
+
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        ParserConfig config = ParserConfig.getInstance();
+        config.loadYml("test/config/parser_KsnXmlDataParserTest.yml");   // テスト用のconfigファイル置き場
+        xpath = XPathFactory.newInstance().newXPath();
+        rule = KsnParseRule.getInstance();
     }
 
     @AfterClass
