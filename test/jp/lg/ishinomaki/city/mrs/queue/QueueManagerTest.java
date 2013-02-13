c0a91ecd53dc007b8469d851336509b9b7366038
ruby-developer <mieno.masayuki@adniss.jp>
2013-02-13 20:10:03 +0900

    テストケース追加

---
+++ test/jp/lg/ishinomaki/city/mrs/queue/QueueManagerTest.java
@@ -0,0 +1,40 @@
+package jp.lg.ishinomaki.city.mrs.queue;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class QueueManagerTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void キューにデータを登録して取り出し() throws Exception {
+        // テスト対象クラス生成
+        QueueManager target = QueueManager.getInstance();
+
+        byte[] data = "testdata".getBytes();
+        target.push(data);
+        assertThat(target.pop(), is(data));
+    }
+
+}
